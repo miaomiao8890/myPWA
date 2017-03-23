9e5c3c9180d0aacf02db56f75758de9bbf5577f9
chis <chis@oupeng.com>
2017-03-23 19:31:11 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,52 @@
   <div id="app">
     <header class="header">
       <div class="inner">
-        <router-link to="/">Home</router-link>
-        <!-- <router-link to="/test">Test</router-link> -->
+        <i class="nav-icon" @click="handleShowNav"></i>
+        <router-link to="/">Beauty</router-link>
       </div>
     </header>
+    <div class="nav">
+      <div :class="[isNavShow ? 'on' : '', 'nav-bg']" @click="handleHideNav"></div>
+      <div :class="[isNavShow ? 'on' : '', 'nav-content']">
+        <div class="nav-content-header"></div>
+        <ul class="nav-content-list">
+          <li><router-link to="/">Pictures</router-link></li>
+          <li><router-link to="/">Album</router-link></li>
+        </ul>
+      </div>
+    </div>
     <transition name="fade" mode="out-in">
       <router-view class="view"></router-view>
     </transition>
   </div>
 </template>
+
+<script>
+
+  export default {
+    components: {
+
+    },
+    computed: {
+    },
+    data() {
+      return {
+        isNavShow: false
+      }
+    },
+    mounted() {
+    },
+    methods: {
+      handleShowNav() {
+        this.isNavShow = true
+      },
+      handleHideNav() {
+        this.isNavShow = false
+      }
+    }
+  }
+
+</script>
 
 <style>
   div, p, dl, dd, h1, h2, h3, h4, h5, h6, a, b, u, em, input, textarea, button, select, form, section, label, span, td, li, nav, footer, body, ul, i { -webkit-box-sizing: border-box; -webkit-tap-highlight-color: rgba(0,0,0,0); padding: 0; margin: 0; }
@@ -33,7 +70,7 @@
     max-width: 640px;
     margin: 0 auto;
     position: relative;
-    margin-top: 52px;
+    margin-top: 3.25rem;
   }
   .header {
     /*background-color: #ff6600;*/
@@ -43,16 +80,78 @@
     left: 0;
     top: 0;
     z-index: 999;
+    box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.19);
+    -webkit-box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.19);
   }
   .header .inner {
-    padding: 10px;
+    padding: .625rem;
+    text-align: center;
+  }
+  .header .inner .nav-icon {
+    width: 1.125rem;
+    height: .75rem;
+    position: absolute;
+    left: 1rem;
+    top: 50%;
+    margin-top: -.375rem;
+    background: url(./assets/images/nav-icon.png) no-repeat;
+    background-size: 1.125rem .75rem;
+    -webkit-background-size: 1.125rem .75rem;
   }
   .header a {
     color: #fff;
-    font-size: 16px;
-    line-height: 32px;
-    margin-right: 10px;
+    font-size: 1.125rem;
+    height: 2rem;
+    line-height: 2rem;
+    margin-right: .625rem;
   }
+  .nav-bg {
+    display: none;
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    background: rgba(0,0,0,.5);
+    z-index: 1000;
+  }
+  .nav-bg.on {
+    display: block;
+  }
+  .nav-content {
+    width: 17.5rem;
+    position: fixed;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    background: #fff;
+    box-shadow: 0px 0px 11px 0px rgba(0, 0, 0, 0.4);
+    -webkit-box-shadow: 0px 0px 11px 0px rgba(0, 0, 0, 0.4);
+    z-index: 1001;
+    transform: translateX(-17.5rem) translateY(0) translateZ(0);
+    -webkit-transform: translateX(-17.5rem) translateY(0) translateZ(0);
+    -webkit-transition: transform .3s cubic-bezier(0, 0, 0.30, 1);
+    -webkit-transition: -webkit-transform .3s cubic-bezier(0, 0, 0.30, 1);
+    transition: -webkit-transform .3s cubic-bezier(0, 0, 0.30, 1);
+    transition: transform .3s cubic-bezier(0, 0, 0.30, 1);
+  }
+  .nav-content.on {
+    transform: translateX(0) translateY(0) translateZ(0);
+    -webkit-transform: translateX(0) translateY(0) translateZ(0);
+  }
+  .nav-content .nav-content-header {
+    width: 100%;
+    height: 9.375rem;
+    background: #260e08;
+  }
+  .nav-content .nav-content-list li a {
+    display: block;
+    font-size: 1rem;
+    text-decoration: underline;
+    padding: 1.25rem;
+    color: #333;
+  }
+
   .fade-enter-active, .fade-leave-active {
     transition: all .2s ease
   }
